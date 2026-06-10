763c95d932ae2fc036973dea1e1ccf70bff82ab3
Sivasankar S <sivasankar.eee2020@gmail.com>
2026-06-11 05:21:24 +0530

    Publish Module: 3d-module v1.0.1 (#6)
    * feat: generated source for test-module v1.0.5
    
    * chore: inject compiled dist artifacts [skip ci]
    
    * feat: generated source for 3d-module v1.0.1
    
    * chore: inject compiled dist artifacts [skip ci]
    
    ---------
    
    Co-authored-by: Rudra CI Bot <ci-bot@rudra.dev>

---
+++ 3d-module/src/module.client.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import { DinoRunner3D } from '@rudra-studio/rudra-three';
+
+export default function CompiledModule(props) {
+
+  return (
+    <div className="rudra-module-wrapper">
+      <DinoRunner3D id="el_1781135148276_0en4xln">
+      </DinoRunner3D>
+    </div>
+  );
+}
