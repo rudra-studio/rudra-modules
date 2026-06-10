fa4989e8c67c64ca9ef63ee2136d14dbe97ecb47
Sivasankar S <sivasankar.eee2020@gmail.com>
2026-06-11 05:24:09 +0530

    Publish Module: header v1.0.1 (#7)
    * feat: generated source for test-module v1.0.5
    
    * chore: inject compiled dist artifacts [skip ci]
    
    * feat: generated source for 3d-module v1.0.1
    
    * chore: inject compiled dist artifacts [skip ci]
    
    * feat: generated source for header v1.0.1
    
    * chore: inject compiled dist artifacts [skip ci]
    
    ---------
    
    Co-authored-by: Rudra CI Bot <ci-bot@rudra.dev>

---
+++ header/src/module.client.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import { Typography } from '@rudra-studio/rudra-core';
+
+export default function CompiledModule(props) {
+
+  return (
+    <div className="rudra-module-wrapper">
+      <Typography id="el_1781135255163_l5j08oy" align="left" weight="normal" content="RUDRA DEMO HEADER" as="p" size="2xl">
+      </Typography>
+    </div>
+  );
+}
