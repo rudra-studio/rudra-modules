1c3dfed181a759b9b09cbb34518f7ae2659077b2
Sivasankar S <sivasankar.eee2020@gmail.com>
2026-06-11 06:45:32 +0530

    Publish Module: ThreeDGame v1.0.1 (#9)
    * feat: generated source for threedgame v1.0.1
    
    * chore: inject compiled dist artifacts [skip ci]
    
    ---------
    
    Co-authored-by: Rudra CI Bot <ci-bot@rudra.dev>

---
+++ threedgame/src/module.client.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import { DinoRunner3D } from '@rudra-studio/rudra-three';
+
+export default function CompiledModule(props) {
+
+  return (
+    <div className="rudra-module-wrapper">
+      <DinoRunner3D id="el_1781140015325_8wip9py">
+      </DinoRunner3D>
+    </div>
+  );
+}
