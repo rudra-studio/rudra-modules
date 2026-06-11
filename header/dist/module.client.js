c8fa373286d58186d9342e1c799a4f79d64f92fb
Simo Rudra <simo.rudra@gmail.com>
2026-06-11 00:20:14 +0000

    feat: generated source for header v1.0.2

--- header/dist/module.client.js
+++
@@ -1,9 +0,0 @@
-import { jsx as r } from "react/jsx-runtime";
-import "react";
-import { Typography as o } from "@rudra-studio/rudra-core";
-function t(e) {
-  return /* @__PURE__ */ r("div", { className: "rudra-module-wrapper", children: /* @__PURE__ */ r(o, { id: "el_1781135255163_l5j08oy", align: "left", weight: "normal", content: "RUDRA DEMO HEADER", as: "p", size: "2xl" }) });
-}
-export {
-  t as default
-};
